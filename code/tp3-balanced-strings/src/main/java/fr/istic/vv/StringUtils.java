52a487a43a372f43cafc1fabc2629725d3f84bd1
knicolle <kelenn.nicolle@etudiant.univ-rennes1.fr>
2023-12-13 17:44:35 +0100

    balanced string + TLS

--- code/tp3-balanced-strings/src/main/java/fr/istic/vv/StringUtils.java
+++ code/tp3-balanced-strings/src/main/java/fr/istic/vv/StringUtils.java
@@ -1,11 +1,30 @@
 package fr.istic.vv;
 
+import java.util.Stack;
+
 public class StringUtils {
 
     private StringUtils() {}
 
     public static boolean isBalanced(String str) {
-        return false;
+        Stack<Character> stack = new Stack<>();
+
+        for (char ch : str.toCharArray()) {
+            if (ch == '{' || ch == '[' || ch == '(') {
+                stack.push(ch);
+            } else if (ch == '}' && !stack.isEmpty() && stack.peek() == '{') {
+                stack.pop();
+            } else if (ch == ']' && !stack.isEmpty() && stack.peek() == '[') {
+                stack.pop();
+            } else if (ch == ')' && !stack.isEmpty() && stack.peek() == '(') {
+                stack.pop();
+            } else {
+
+            }
+        }
+
+        return stack.isEmpty();
     }
+
 
 }
